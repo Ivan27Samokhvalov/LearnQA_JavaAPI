2daf7c7631f008b796944d45beb11bb720459ccd
ivansamokhvalov <Ivan.Samohvalov@leroymerlin.ru>
2026-03-06 23:54:52 +0300

    add test getJsonHomeWorkTest

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,17 +1,18 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-public class HelloFromIvan {
+public class ApiTests {
 
     @Test
-    public void getJsonHomeWorkTest() {
+    public void jsonParsingTest() {
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .andReturn()
                 .jsonPath();
 
-        System.out.println(response.prettyPrint());
+        String secondMessageText = response.getString("messages[1].message");
+
+        System.out.println(secondMessageText);
     }
 }
