32f5b5d45c6e0c3744195a372999461fde044ad7
ivansamokhvalov <Ivan.Samohvalov@leroymerlin.ru>
2026-03-06 18:03:50 +0300

    add test getJsonHomeWorkTest

--- src/test/java/HelloFromIvan.java
+++ src/test/java/HelloFromIvan.java
@@ -1,14 +1,17 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloFromIvan {
 
     @Test
-    public void helloFromIvan() {
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
+    public void getJsonHomeWorkTest() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .andReturn()
+                .jsonPath();
+
+        System.out.println(response.prettyPrint());
     }
 }
